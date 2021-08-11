2b939259bd94069e5ad7d4f316a0d98b94811c30
shota-aa <dqx1005@icloud.com>
2021-08-11 13:14:59 +0900

    todolist作る前

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <hello-world msg="Hello Vue 3 + Vite" />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+// ここでexportするのって全部をコンポーネントとして？
+export default {
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
