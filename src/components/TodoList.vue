a6ce25da0b3df42d99157be6b61561b52575b64d
shota-aa <dqx1005@icloud.com>
2021-08-16 01:23:39 +0900

    優先順位の自動設定、変更、順番に表時

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -10,10 +10,11 @@
   </div>
   <br />
   <div>未完了のタスク</div>
-  <div v-for="item in items" :key="item.todo">
+  <div v-for="item in sortedItems" :key="item.todo">
     <div v-if="item.status == false">
-      <a>{{ item.name }} &nbsp;</a>
+      <a>{{ item.priority }}: {{ item.name }} &nbsp;</a>
       <button @click="changeToCompleted(item)">↑</button>
+      <button @click="changePriority(item)">priority</button>
       <br />
     </div>
   </div>
@@ -21,6 +22,8 @@
     追加するタスクの名前
     <input v-model="newItemName" type="text" />
     <button @click="addItem">add</button>
+    優先順位入力
+    <input v-model.number="inputPriority" type="number" />
   </label>
 </template>
 
@@ -31,21 +34,57 @@ export default {
   name: "TodoList",
   setup() {
     const items = ref([
-      { name: "お菓子買う", status: true },
-      { name: "寝る", status: false },
+      { name: "お菓子買う", status: true, priority: 0 },
+      { name: "寝る", status: false, priority: 1 },
     ]);
     const newItemName = ref("");
+    const inputPriority = ref(0);
 
     const addItem = () => {
-      items.value.push({ name: newItemName.value, status: false });
+      items.value.push({
+        name: newItemName.value,
+        status: false,
+        priority:
+          Math.max.apply(
+            null,
+            items.value.map(function (i) {
+              return i.priority;
+            })
+          ) + 1,
+      });
+
       newItemName.value = "";
     };
 
+    var sortedItems = items.value.sort(function (a, b) {
+      return a.priority - b.priority;
+    });
     const changeToUncompleted = (item) => {
       item.status = false;
+      item.priority =
+        Math.max.apply(
+          null,
+          items.value.map(function (i) {
+            return i.priority;
+          })
+        ) + 1;
+      sortedItems = items.value.sort(function (a, b) {
+        return a.priority - b.priority;
+      });
     };
     const changeToCompleted = (item) => {
       item.status = true;
+      item.priority = 0;
+      sortedItems = items.value.sort(function (a, b) {
+        return a.priority - b.priority;
+      });
+    };
+    const changePriority = (item) => {
+      item.priority = inputPriority.value;
+      inputPriority.value = 0;
+      sortedItems = items.value.sort(function (a, b) {
+        return a.priority - b.priority;
+      });
     };
 
     return {
@@ -54,6 +93,9 @@ export default {
       addItem,
       changeToCompleted,
       changeToUncompleted,
+      changePriority,
+      inputPriority,
+      sortedItems,
     };
   },
 };
